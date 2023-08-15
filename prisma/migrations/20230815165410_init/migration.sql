4827fe4e7a8eeac3281c9f0b4b79f1971e1e61c3
ekorber <erickorber1994@gmail.com>
2023-08-15 12:56:21 -0400

    Setup dev database

---
+++ prisma/migrations/20230815165410_init/migration.sql
@@ -0,0 +1,8 @@
+-- CreateTable
+CREATE TABLE "Product" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "title" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "price" INTEGER NOT NULL,
+    "inStock" BOOLEAN NOT NULL DEFAULT true
+);
