0d758b3bb22f27640034684ef319a5085d84d812
ekorber <erickorber1994@gmail.com>
2023-08-16 01:57:32 -0400

    Populate page with components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,15 @@
+
+import PrimaryButton from "@/components/primaryButton"
+import ProductList from "@/components/productList"
 import Header from "@/components/header"
 
 export default function Home() {
   return (
     <>
       <Header />
+      <ProductList />
+      <PrimaryButton buttonText="Add to Cart" />
+      <PrimaryButton buttonText="Something Similar..." />
     </>
   )
 }
