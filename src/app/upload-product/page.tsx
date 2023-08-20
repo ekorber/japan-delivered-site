2f9266f9e716c13d59f890cde186a9fcee6ecde6
ekorber <erickorber1994@gmail.com>
2023-08-20 00:08:36 -0400

    Styled grid layout for upload product page

--- src/app/upload-product/page.tsx
+++ src/app/upload-product/page.tsx
@@ -1,4 +1,5 @@
 import { prisma } from "@/db"
+import styles from '@/styles/productUploadForm.module.css'
 
 export async function submitProduct(data: FormData) {
     "use server"
@@ -58,42 +59,58 @@ export async function submitProduct(data: FormData) {
 export default function UploadProductPage() {
     return (
         <>
-            <h1 className="text-center mt-10 text-3xl">Upload A New Product</h1>
-            <form action={submitProduct} method="POST" className="text-center my-10">
+            <h1 className="text-center my-20 text-3xl">Upload A New Product</h1>
+            <form action={submitProduct} method="POST" className={styles.formLayout}>
 
-                <label htmlFor="title" className="block">Product Title</label>
-                <input name="title" type="text" placeholder="Enter Title" className="text-center border-2 border-black" required />
+                <div className={styles.title}>
+                    <label htmlFor="title" className="block">Product Title</label>
+                    <input name="title" type="text" placeholder="Enter Title" className="text-center border-2 border-black w-full" required />
+                </div>
 
-                <label htmlFor="description" className="block mt-7">Product Description</label>
-                <textarea name="description" placeholder="Enter Description" className="text-center border-2 border-black" required />
+                <div className={styles.description}>
+                    <label htmlFor="description" className="block">Product Description</label>
+                    <textarea name="description" placeholder="Enter Description" className="text-center border-2 border-black w-full" required />
+                </div>
 
-                <label htmlFor="tags" className="block mt-7">Product Tags</label>
-                <input name="tags" placeholder="nintendo, joycon, zelda" className="text-center border-2 border-black" required />
-                <p>(relevant for user searching and being offered related products)</p>
-                <p className="text-blue-700">Seperate them with a comma.</p>
-                <p className="text-blue-700">30 characters max per tag.</p>
-                <p className="text-blue-700">8 tags max.</p>
+                <div className={styles.stock}>
+                    <label htmlFor="stock" className="block">Available Stock (Optional)</label>
+                    <input name="stock" type="number" placeholder="Enter Stock" className="text-center border-2 border-black w-full" />
+                </div>
 
-                <label htmlFor="price-jpy" className="block mt-7">Product Price (JPY)</label>
-                <input name="price-jpy" type="number" placeholder="Enter Japanese Price" className="text-center border-2 border-black" required />
+                <div className={styles.tags}>
+                    <label htmlFor="tags" className="block">Product Tags</label>
+                    <input name="tags" placeholder="nintendo, joycon, zelda" className="text-center border-2 border-black w-full" required />
+                    <p>(relevant for user searching and being offered related products)</p>
+                    <p className="text-blue-700">Seperate them with a comma.</p>
+                    <p className="text-blue-700">30 characters max per tag.</p>
+                    <p className="text-blue-700">8 tags max.</p>
+                </div>
 
-                <label htmlFor="price-cad" className="block mt-7">Product Price (CAD)</label>
-                <input name="price-cad" type="number" placeholder="Enter Canadian Price" className="text-center border-2 border-black" required />
-                <p className="text-white bg-blue-700">Price MUST BE in cents.</p>
+                <div className={styles.priceJPY}>
+                    <label htmlFor="price-jpy" className="block">Product Price (JPY)</label>
+                    <input name="price-jpy" type="number" placeholder="Enter Japanese Price" className="text-center border-2 border-black w-full" required />
+                </div>
 
-                <label htmlFor="price-usd" className="block mt-7">Product Price (USD) IN CENTS</label>
-                <input name="price-usd" type="number" placeholder="Enter American Price" className="text-center border-2 border-black" required />
-                <p className="text-white bg-blue-700">Price MUST BE in cents.</p>
+                <div className={styles.priceCAD}>
+                    <label htmlFor="price-cad" className="block">Product Price (CAD)</label>
+                    <input name="price-cad" type="number" placeholder="Enter Canadian Price" className="text-center border-2 border-black w-full" required />
+                    <p className="text-blue-700">Price MUST BE in cents.</p>
+                </div>
 
-                <label htmlFor="price-gbp" className="block mt-7">Product Price (GBP) IN CENTS</label>
-                <input name="price-gbp" type="number" placeholder="Enter British Price" className="text-center border-2 border-black" required />
-                <p className="text-white bg-blue-700">Price MUST BE in cents.</p>
+                <div className={styles.priceUSD}>
+                    <label htmlFor="price-usd" className="block">Product Price (USD) IN CENTS</label>
+                    <input name="price-usd" type="number" placeholder="Enter American Price" className="text-center border-2 border-black w-full" required />
+                    <p className="text-blue-700">Price MUST BE in cents.</p>
+                </div>
 
-                <label htmlFor="stock" className="block mt-7">Available Stock (Optional)</label>
-                <input name="stock" type="number" placeholder="Enter Stock" className="text-center border-2 border-black" />
+                <div className={styles.priceGBP}>
+                    <label htmlFor="price-gbp" className="block">Product Price (GBP) IN CENTS</label>
+                    <input name="price-gbp" type="number" placeholder="Enter British Price" className="text-center border-2 border-black w-full" required />
+                    <p className="text-blue-700">Price MUST BE in cents.</p>
+                </div>
 
-                <div className="mt-10">
-                    <button type="submit" className="bg-black hover:bg-blue-700 text-white px-5 py-3">Upload product to database</button>
+                <div className={styles.submitButton}>
+                    <button type="submit" className="bg-black hover:bg-blue-700 text-white px-10 py-6">Upload product to database</button>
                 </div>
             </form>
         </>
