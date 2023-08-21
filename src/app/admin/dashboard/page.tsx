a1f7b88a00706a9ec43c34ac146833a59cc95f66
ekorber <erickorber1994@gmail.com>
2023-08-20 21:38:32 -0400

    Create empty admin pages

---
+++ src/app/admin/dashboard/page.tsx
@@ -0,0 +1,7 @@
+export default function AdminDashboardPage() {
+    return (
+        <>
+            <h1 className="text-center mt-12 mb-20 text-3xl">Dashboard</h1>
+        </>
+    );
+}
---
+++ src/app/admin/login/page.tsx
@@ -0,0 +1,7 @@
+export default function LoginPage() {
+    return (
+        <>
+            <h1 className="text-center mt-12 mb-20 text-3xl">Login</h1>
+        </>
+    );
+}
