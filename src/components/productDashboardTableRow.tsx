4fb0147b8e28f8dd71ecab9a4dd429099dfa628c
ekorber <erickorber1994@gmail.com>
2023-08-22 13:59:49 -0400

    Optimisically remove item from view, when delete btn pressed

--- src/components/productDashboardTableRow.tsx
+++ src/components/productDashboardTableRow.tsx
@@ -2,8 +2,12 @@
 
 import { Product } from "@prisma/client";
 import AdminButton from "./adminButton";
+import { Dispatch, SetStateAction, useState } from "react";
 
-export async function deleteProduct(id: string, removeProduct: Function) {
+export async function deleteProduct(id: string, removeProduct: Function, setEnabled: Dispatch<SetStateAction<boolean>>) {
+
+    setEnabled(false)
+
     const res = await fetch('/admin/dashboard/product/', {
         method: 'DELETE',
         headers: {
@@ -25,8 +29,14 @@ type ProductDashboardTableRowProps = {
 }
 
 export default function ProductDashboardTableRow({ product, removeProduct }: ProductDashboardTableRowProps) {
+    const [enabled, setEnabled] = useState(true)
+
+    let displayValue = ''
+    if (!enabled)
+        displayValue = 'hidden'
+
     return (
-        <tr className="even:bg-blue-300">
+        <tr className={`${displayValue} even:bg-blue-300`}>
             <td className="pl-2 border border-black">{product.title}</td>
             <td className="pl-2 border border-black">{product.description}</td>
             <td className="pl-2 border border-black">{product.stock}</td>
@@ -37,7 +47,7 @@ export default function ProductDashboardTableRow({ product, removeProduct }: Pro
             <td className="pl-2 border border-black">{(product.priceGBP / 100).toFixed(2)}</td>
             <td className="pl-2 border border-black">
                 <AdminButton buttonText="Edit" onClickFunction={() => console.log('Editing: ' + product.id)} className="p-2 m-1" />
-                <AdminButton buttonText="Delete" onClickFunction={() => deleteProduct(product.id, removeProduct)} className="p-2 m-1" />
+                <AdminButton buttonText="Delete" onClickFunction={() => deleteProduct(product.id, removeProduct, setEnabled)} className="p-2 m-1" />
             </td>
         </tr>
     );
