5c36cc2291dbd7c9c7259c3c35f3ba33c087749f
ekorber <erickorber1994@gmail.com>
2023-08-22 14:05:58 -0400

    Include product ordering in list request

--- src/app/admin/dashboard/product-list/route.ts
+++ src/app/admin/dashboard/product-list/route.ts
@@ -3,7 +3,10 @@ import { prisma } from "@/db";
 export async function GET() {
     const products = await prisma.product.findMany({
         skip: 0,
-        take: 10
+        take: 10,
+        orderBy: {
+            'title': 'asc'
+        }
     })
 
     return new Response(JSON.stringify(products))
