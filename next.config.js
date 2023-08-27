baf985b3f97fe1afc2e272c1c7be876503d31385
ekorber <erickorber1994@gmail.com>
2023-08-26 22:13:26 -0400

    Allow image urls from s3 bucket

--- next.config.js
+++ next.config.js
@@ -2,7 +2,14 @@
 const nextConfig = {
     experimental: {
         serverActions: true,
-    }
+    },
+    webp: {
+        preset: "default",
+        quality: 100,
+    },
+    images: {
+        domains: ["japan-delivered-product-image-bucket.s3.us-east-1.amazonaws.com"]
+    },
 }
 
 module.exports = nextConfig
