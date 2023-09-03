4a1894c72acdd333ff6a2ea2fbdc6206f932ff96
ekorber <erickorber1994@gmail.com>
2023-09-03 19:56:04 -0400

    Add extra default colours

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,10 @@ const config: Config = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      colors: {
+        'primary-red': '#bc002d',
+        'primary-red-dark': '#8d0021',
+      },
     },
   },
   plugins: [],
