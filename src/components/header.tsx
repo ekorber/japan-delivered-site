1c7549fdbe94614f04fca84a7f4a3c0b8b4cb93f
ekorber <erickorber1994@gmail.com>
2023-09-04 10:18:36 -0400

    Replace loaded icons with direct SVG

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -2,9 +2,19 @@ export default function Header() {
     return (
         <>
             <header className='w-full h-12 py-2.5 flex bg-primary-red justify-center'>
-                <button className='bg-primary-red text-center text-white basis-16 relative border-none mt-[-1px] text-2xl'><i className="fa-solid fa-bars  hover:duration-200 hover:scale-125"></i></button>
-                <input className='max-w-lg grow' type="search" placeholder=" What would you like from Japan?" />
-                <button className='bg-primary-red text-center text-white basis-16 relative border-none mt-[-2px] text-2xl'><i className="fa-solid fa-cart-shopping hover:duration-200 hover:scale-125"></i></button>
+                <button className='bg-primary-red basis-8 mt-[-2px] mx-3'>
+                    <svg width="32" height="32" fill="white" className="hover:duration-200 hover:scale-125" viewBox="0 0 16 16">
+                        <path d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z" />
+                    </svg>
+                </button>
+
+                <input className='max-w-lg grow' type="search" placeholder="  What would you like from Japan?" />
+
+                <button className='bg-primary-red basis-6 mt-[-2px] mx-3'>
+                    <svg width="26" height="26" fill="white" className="hover:duration-200 hover:scale-125" viewBox="0 0 16 16">
+                        <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2z" />
+                    </svg>
+                </button>
             </header>
         </>
     );
