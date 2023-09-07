53868815329a5d1ac7693d1f05220ca1628e8e76
ekorber <erickorber1994@gmail.com>
2023-09-07 15:23:11 -0400

    Create empty cart page

---
+++ src/app/cart/page.tsx
@@ -0,0 +1,13 @@
+import HeaderNoSearchbar from "@/components/headerNoSearchbar";
+
+export default function Cart() {
+    return (
+        <>
+            <HeaderNoSearchbar />
+            <div className="px-10 my-10">
+                <h2 className="text-3xl text-center mb-5">Your Loot</h2>
+
+            </div>
+        </>
+    )
+}
