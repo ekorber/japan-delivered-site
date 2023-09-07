9245c6951ee2baa3ceb36b856332544ca70c50c7
ekorber <erickorber1994@gmail.com>
2023-09-07 14:52:09 -0400

    Create custom-request page (no form yet)

---
+++ src/app/custom-request/page.tsx
@@ -0,0 +1,13 @@
+import Header from "@/components/header"
+
+export default function CustomRequest() {
+    return (
+        <>
+            <Header />
+            <div className="px-10 my-10">
+                <h2 className="text-3xl text-center mb-5">Tell Us What You'd Like From Japan!</h2>
+                <h4 className="text-xl text-center mb-10">You can request products here that may not be available directly on our site. We'll get back to you shortly, letting you know if we're able to fulfill your request, and how much it'll cost.</h4>
+            </div>
+        </>
+    )
+}
