8bcbc8baa28179a3e7fd046b0a27c4069718e573
ekorber <erickorber1994@gmail.com>
2023-09-07 15:25:43 -0400

    Replace built-in header with header component

--- src/app/checkout/page.tsx
+++ src/app/checkout/page.tsx
@@ -1,18 +1,9 @@
-import { Shojumaru } from 'next/font/google'
-
-const shojumaru = Shojumaru({
-    weight: '400',
-    subsets: ['latin'],
-    display: 'swap'
-})
+import HeaderNoSearchbar from "@/components/headerNoSearchbar";
 
 export default function CustomRequest() {
-
     return (
         <>
-            <header className='w-full h-12 bg-primary-red text-white'>
-                <p className={`${shojumaru.className} pt-3 pl-5`}>Japan Delivered</p>
-            </header>
+            <HeaderNoSearchbar />
             <div className="px-10 my-10">
                 <h2 className="text-3xl text-center mb-5">Checkout</h2>
 
