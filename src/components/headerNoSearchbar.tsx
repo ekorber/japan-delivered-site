42be146bd9038e334c076336369743a4d8e2dc4a
ekorber <erickorber1994@gmail.com>
2023-09-08 11:26:44 -0400

    Create Cart Icon Button for headers

--- src/components/headerNoSearchbar.tsx
+++ src/components/headerNoSearchbar.tsx
@@ -1,3 +1,4 @@
+import CartIconButton from '@/features/products/components/cartIconButton'
 import { Shojumaru } from 'next/font/google'
 
 const shojumaru = Shojumaru({
@@ -17,11 +18,7 @@ export default function HeaderNoSearchbar() {
 
             <p className={`${shojumaru.className} pt-0.5 pl-5 text-white grow`}>Japan Delivered</p>
 
-            <button className='bg-primary-red basis-6 mt-[-2px] mx-3'>
-                <svg width="26" height="26" fill="white" className="hover:duration-200 hover:scale-125" viewBox="0 0 16 16">
-                    <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2z" />
-                </svg>
-            </button>
+            <CartIconButton />
         </header>
     )
 }
---
+++ src/features/products/components/cartIconButton.tsx
@@ -0,0 +1,13 @@
+import Link from "next/link";
+
+export default function CartIconButton() {
+    return (
+        <Link href='/cart'>
+            <button className='bg-primary-red basis-6 mt-[-2px] mx-3'>
+                <svg width="26" height="26" fill="white" className="hover:duration-200 hover:scale-125" viewBox="0 0 16 16">
+                    <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2z" />
+                </svg>
+            </button>
+        </Link >
+    )
+}
